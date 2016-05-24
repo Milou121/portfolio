8a50686860526141dbc6d2c678f093af49731a79
Milou Schelling <milou.schelling@gmail.com>
2016-05-24 17:49:31 +0200

    portfolio updated may 24

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,27 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+    // $('#about').removeClass('.navbar-link').addClass('.navbar-link-darker');
+    // $(document.parent.document).scroll(function() {
+    // });
+
+
+    $(document).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    console.log($(document).scrollTop())
+    if ($(document).scrollTop() > 566) {
+      $('.navbar').removeClass('navbar-top').addClass('navbar-content', 900);
+    }
+    if ($(document).scrollTop() < 565) {
+      $('.navbar').removeClass('navbar-content').addClass('navbar-top');
+    }
+  });
+
+    // $('.resume-buttons').click(function(){
+    //   $("p").removeClass('active').next().addClass('active');
+    // });
+  });
